19f4e4329022193835ed803b83101f48ead8d057
staneee <yi.hang@live.com>
2020-10-13 20:18:25 +0800

    修改nuget打包输出目录名称

--- nupkg/common.ps1
+++ nupkg/common.ps1
@@ -1,7 +1,7 @@
 # 路径
 $packFolder = (Get-Item -Path "./" -Verbose).FullName   # 当前路径
 $rootFolder = Join-Path $packFolder "../"               # 项目根目录
-$packOutputFolder = Join-Path $packFolder "framework"   # 输出nuget package 目录
+$packOutputFolder = Join-Path $packFolder "dist"   # 输出nuget package 目录
 
 
 
